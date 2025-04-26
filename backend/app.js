298e7a11e615832c43d3ee47992550a161f64460
Daminikesharkar <kesharkardamini1234@gmail.com>
2025-04-26 14:19:31 +0530

    Set up database connection and Created the User model

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 
+const sequelize = require("./util/database");
+
 const app = express();
 
+sequelize.sync();
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Backend Server Started on Port: ",process.env.PORT || 3000);
 })
