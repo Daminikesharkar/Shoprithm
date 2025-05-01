d569230a4d5cd36a576195d6b095da4200ec2179
Daminikesharkar <kesharkardamini1234@gmail.com>
2025-05-01 11:00:51 +0530

    set up routing and basic signin and signup page structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <h1>Welcome to Shoprithm</h1>
+      <Link to="/signin">Login</Link> | <Link to="/signup">Signup</Link>
     </>
   )
 }
