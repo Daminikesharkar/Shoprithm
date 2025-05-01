5beb003aa89160c37d91db6c4775d1d5e0602caa
Daminikesharkar <kesharkardamini1234@gmail.com>
2025-05-01 10:13:58 +0530

    setup frontend with Vite + React

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,3 +4,4 @@ const userController = require('../controllers/user');
 const router = express.Router();
 
 router.post('/signUp', userController.signUp);
+router.post('/signIn',userController.signIn);
