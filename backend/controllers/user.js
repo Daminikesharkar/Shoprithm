164f0d1019426faacb7a7be4f80c0dab3513d0d2
Daminikesharkar <kesharkardamini1234@gmail.com>
2025-05-01 10:05:20 +0530

    Implement user login with JWT and password validation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const Users = require('../models/user');
 
 
@@ -35,4 +36,37 @@ exports.signUp = async(req, res)=>{
         })
     }
 
+}
+
+exports.signIn = async(req,res)=>{
+    try {
+        const {email,password} = req.body;
+
+        const user =  await Users.findOne({where: {email:email}});
+
+        if(user){
+            const passwordMatch = await bcrypt.compare(password,user.password);
+
+            if(passwordMatch){
+                const token = jwt.sign({userId:user.id},process.env.SECRETKEY,{expiresIn:'1h'});
+                res.status(200).json({
+                    message:'User logged in sucessfully',
+                    token:token,
+                    user:user
+                })
+            }else{
+                return res.status(400).json({
+                    message:'password doesnt match, please try again'
+                })
+            }
+        }else{
+            return res.status(400).json({
+                message:'User does not exists please sign-up first'
+            })
+        }        
+    } catch (error) {
+        return res.status(500).json({
+            error:`Something went wrong: ${error}`
+        })
+    }
 }
